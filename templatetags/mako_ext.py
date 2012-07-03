bc2129bd5fafc6d54b268527d90cac8e6ea33d3a
Zhang <clvrobj@gmail.com>
2012-07-03 16:33:21 +0800

    can receive multiple key params

--- templatetags/mako_ext.py
+++ templatetags/mako_ext.py
@@ -4,8 +4,9 @@ from djangomako.shortcuts import render_to_response
 register = Library()
 
 @register.simple_tag(takes_context=True)
-def include_mako_template(context, template):
+def include_mako_template(context, template, **kwargs):
     context_dict = {}
     for c in context:
         context_dict.update(c)
+    context_dict.update(kwargs)
     return render_to_response(template, context_dict).content
